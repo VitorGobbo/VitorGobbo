07d5fd4c4ccca115456bc0b8d64afe30489fdeca
VitorGobbo <vitor6325@gmail.com>
2023-11-28 00:03:49 -0300

    inicializacao do projeto

--- exercicio1.js
+++ exercicio1.js
@@ -1,5 +1,3 @@
-// Escreva um programa que declare duas variáveis, “nome” e “idade”, 
-//e as imprima em um console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
- let nome = "Vitor";
+let nome = "Vitor";
  let idade = "23"
- console.log("Olá meu nome é "+ nome + ", eu tenho "+ idade + " anos de idade.")
+" console.log("Olá meu nome é "+ nome + ", eu tenho "+ idade + " anos de idade.")"
