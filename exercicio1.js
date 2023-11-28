39a6d94b5326d3c57cbf88fb515698deba76ba17
VitorGobbo <vitor6325@gmail.com>
2023-11-27 23:47:11 -0300

    meu primeiro commit

---
+++ exercicio1.js
@@ -0,0 +1,5 @@
+// Escreva um programa que declare duas variáveis, “nome” e “idade”, 
+//e as imprima em um console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
+ let nome = "Vitor";
+ let idade = "23"
+ console.log("Olá meu nome é "+ nome + ", eu tenho "+ idade + " anos de idade.")
